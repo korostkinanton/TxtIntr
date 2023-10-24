440f82a13e97996d1eedccc7311c08e93ebe4bde
korostkinanton <145912988+korostkinanton@users.noreply.github.com>
2023-10-24 17:30:23 +0300

    Update calc.cpp

--- calculator/calc.cpp
+++ calculator/calc.cpp
@@ -2,42 +2,38 @@
 #include <math.h>
 #include <unistd.h>
 using namespace std;
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
     if(argc == 1){
         cout << "Тригонометрический калькулятор" << endl
         << "Этот калькулятор нужен для рассчета арктангенса и аркотангенса в радианах и градусах" << endl
-        << "Введите число c параметром -x" << endl
-        << "Чтобы найти арктангенс введите параметр 't'" << endl
-        << "Чтобы найти аркотангенс введие параметр 'c'" << endl
-        << "Пример использования ./hello -x 30 -o 't'" << endl;
+        << "Чтобы найти арктангенс введите параметр -t" << endl
+        << "Чтобы найти аркотангенс введие параметр -c" << endl
+        << "Пример использования ./hello -t 30" << endl;
         return 0;
     }
-    //char* opts = "x:o:";
-    double x;
-    char op;
     int opt;
-    while((opt = getopt(argc, argv, "x:o:")) != -1){
-        switch(opt)
-        {
-            case 'x':
-            x = atoi(optarg);
+    double result = 0, x;
+    while((opt = getopt(argc, argv, "t:c:")) != -1){
+        switch(opt){
+            case 't':
+                x = strtod(argv[2],NULL);
+                result = atan(x);
+                cout << "arctg" << " " << x << " " << "в радианах равен..." << result << endl;
+                result = atan(x)*180/M_PI;
+                cout << "arctg" << " " << x << " " << "в градусах равен..." << result << endl;
             break;
-            case 'o':
-            op = optarg[0];
+            case 'c':
+                x = strtod(argv[2],NULL);
+                result = M_PI_2-atan(x);
+                cout << "arcctg" << " " << x << " " << "в радианах равен..." << M_PI_2-atan(x) << endl;
+                result = (M_PI_2-atan(x))*180/M_PI;
+                cout << "arcctg" << " " << x << " " << "в радианах равен..." << (M_PI_2-atan(x))*180/M_PI << endl;
             break;
         }
-    }
-    switch(op){
-        case 't':
-        cout << "arctg" << " " << x << " " << "в радианах равен..." << atan(x) << endl;
-        cout << "arctg" << " " << x << " " << "в градусах равен..." << atan(x)*180/M_PI << endl;
-        break;
-        case 'c':
-        cout << "arcctg" << " " << x << " " << "в радианах равен..." << M_PI_2-atan(x) << endl;
-        cout << "arcctg" << " " << x << " " << "в радианах равен..." << (M_PI_2-atan(x))*180/M_PI << endl;
-        break;
+        
     }
     return 0;
-};
+}
+
 
